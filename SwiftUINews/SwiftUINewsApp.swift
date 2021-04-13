cd4ed6d5a7dd5673008ac383149072bdf9d35122
Richard Price <mrspondooli@gmail.com>
2021-04-13 21:53:04 +0100

    project setup

---
+++ SwiftUINews/SwiftUINewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINewsApp.swift
+//  SwiftUINews
+//
+//  Created by Richard Price on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
